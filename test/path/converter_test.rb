86d972fc775040d65b6204e3e9c7b907e0650291
Thiago A. Silva <thiagoaraujos@gmail.com>
2014-07-18 15:11:47 -0300

    Split path tests

---
+++ test/path/converter_test.rb
@@ -0,0 +1,43 @@
+module MenuMaker
+  class PathConverterTest < ActionView::TestCase
+    test 'creates a get path from a given string' do
+      assert_equal Path.new(:get, '/path'), Path::Converter.convert('/path')
+    end
+
+    test 'creates a path from an array' do
+      assert_equal Path.new(:post, '/path'), Path::Converter.convert([:post, '/path'])
+    end
+
+    test "create path from array assumes GET by default" do
+      assert_equal Path.new(:get, '/path'), Path::Converter.convert(['/path'])
+    end
+
+    test "create path from array creates an empty path, when given an empty array" do
+      assert_equal Path.new(:get, ''), Path::Converter.convert([])
+    end
+
+    test "returns back if path is already a Path" do
+      path = Path.new :put, '/path'
+      assert_equal path, Path::Converter.convert(path)
+    end
+
+    test "creates a path from any object which responds to path and method" do
+      request = Class.new do
+        def path; '/path' end
+        def method; 'PUT' end
+      end.new
+
+      assert_equal Path.new(:put, '/path'), Path::Converter.convert(request)
+    end
+
+    test "fails if can't create path from object which responds to path and method" do
+      assert_raise Path::PathError do
+        Path::Converter.convert(Object.new)
+      end
+    end
+
+    test "a Path global conversion method is also available" do
+      assert_equal Path.new(:post, '/path'), Path(:post, '/path')
+    end
+  end
+end
--- test/path_test.rb
+++ test/path_test.rb
@@ -18,61 +18,25 @@ module MenuMaker
       assert_equal '/path', Path.new(:get, '/path').to_s
     end
 
-    test 'creates a get path from a given string' do
-      assert_equal Path.new(:get, '/path'), Path::Converter.convert('/path')
-    end
-
-    test 'creates a path from an array' do
-      assert_equal Path.new(:post, '/path'), Path::Converter.convert([:post, '/path'])
-    end
-
-    test "creates a path from an array assumes get when can't find request method" do
-      assert_equal Path.new(:get, '/path'), Path::Converter.convert(['/path'])
-    end
-
-    test "creates a path from an array on empty array creates empty get path" do
-      assert_equal Path.new(:get, ''), Path::Converter.convert([])
-    end
-
-    test "returns back the path if already a Path" do
-      path = Path.new :put, '/path'
-      assert_equal path, Path::Converter.convert(path)
-    end
-
-    test "creates a path from an object wich responds to path and method" do
-      request = Class.new do
-        def path
-          '/path'
-        end
-
-        def method
-          'PUT'
-        end
-      end.new
-
-      assert_equal Path.new(:put, '/path'), Path::Converter.convert(request)
-    end
-
-    test "fails if can't create path from object which responds to path and method" do
-      assert_raise Path::PathError do
-        Path::Converter.convert(Object.new)
-      end
-    end
-
-    test "equality works with an equal path" do
+    test "equality works with another Path object" do
       assert_equal Path.new(:put, '/path'), Path.new(:put, '/path')
     end
 
-    test "equality works with a convertible string" do
+    test "equality works with a compliant string" do
       assert_equal Path.new(:get, '/path'), '/path'
     end
 
-    test "equality works with a convertible array" do
+    test "equality works with a compliant array" do
       assert Path.new(:post, '/path') == [:post, '/path']
     end
 
-    test "a Path global conversion method is also available" do
-      assert_equal Path.new(:post, '/path'), Path(:post, '/path')
+    test "equality works with other compliant objects" do
+      object = Class.new do
+        def path; '/path' end
+        def method; 'PUT' end
+      end.new
+
+      assert_equal Path.new(:put, '/path'), object
     end
   end
 end
