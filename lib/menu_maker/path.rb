748da5626a3fd5c77febebceca128329abd1cdb1
Thiago A. Silva <thiagoaraujos@gmail.com>
2014-07-18 00:46:01 -0300

    Introduce a Path class

---
+++ lib/menu_maker/path.rb
@@ -0,0 +1,50 @@
+module MenuMaker
+  PathError = Class.new StandardError
+
+  def Path(path)
+    klass  = path.class.name.split('::').last.downcase
+    method = :"from_#{klass}"
+
+    fail PathError unless Path.respond_to? method
+
+    Path.send method, path
+  end
+
+  class Path
+    METHODS = %i[get post put patch delete]
+
+    attr_reader :method, :address
+
+    def self.from_string(address)
+      Path.new(:get, address.to_s)
+    end
+
+    def self.from_array(path)
+      has_method = proc { |el| METHODS.include? el }
+
+      method  = path.find(&has_method) || :get
+      address = path.delete_if(&has_method).first
+
+      new method, address
+    end
+
+    def self.from_path(path)
+      path if path.is_a?(Path) or fail PathError
+    end
+
+    def initialize(method, address)
+      fail PathError unless METHODS.include? method
+
+      @method  = method
+      @address = address.to_s
+    end
+
+    def ==(other)
+      method == other.method && address == other.address
+    end
+
+    def to_s
+      address
+    end
+  end
+end
---
+++ test/path_test.rb
@@ -0,0 +1,48 @@
+require 'menu_maker/path'
+
+module MenuMaker
+  class PathTest < ActionView::TestCase
+    test 'accepts get, post, put, patch and delete' do
+      %i[get post put patch delete].each do |method|
+        Path.new method, '/path'
+      end
+    end
+
+    test "doesn't accept a method other than get, post, put, patch and delete" do
+      assert_raise PathError do
+        Path.new :ouch, '/path'
+      end
+    end
+
+    test 'to_s returns the address' do
+      assert_equal '/path', Path.new(:get, '/path').to_s
+    end
+
+    test 'creates a get path from a given string' do
+      assert_equal Path.new(:get, '/path'), Path('/path')
+    end
+
+    test 'creates a path from an array' do
+      assert_equal Path.new(:post, '/path'), Path([:post, '/path'])
+    end
+
+    test "creates a path from an array assumes get when can't find request method" do
+      assert_equal Path.new(:get, '/path'), Path(['/path'])
+    end
+
+    test "creates a path from an array on empty array creates empty get path" do
+      assert_equal Path.new(:get, ''), Path([])
+    end
+
+    test "returns back the path if already a Path" do
+      path = Path.new :put, '/path'
+      assert_equal path, Path(path)
+    end
+
+    test "from_path fails when not a path" do
+      assert_raise PathError do
+        Path.from_path(Object.new)
+      end
+    end
+  end
+end
