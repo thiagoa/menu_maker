e4b7233298a4c4daa551f3a26f454225de97c4fb
Thiago A. Silva <thiagoaraujos@gmail.com>
2014-07-19 17:58:10 -0300

    Unit tests covering MenuRenderer functionality, which was already covered by integration

--- test/menu_renderer_test.rb
+++ test/menu_renderer_test.rb
@@ -3,27 +3,102 @@ require 'ostruct'
 
 module MenuMaker
   class MenuRendererTest < ActiveSupport::TestCase
-    context 'supplied context has a request' do
-      ContextStub = begin
-        request_stub = Class.new do
-          def method; 'POST'  end
-          def path;   '/path' end
-        end.new
+    ChildMenuRenderer = Class.new MenuRenderer do
+      render {}
+    end
 
-        OpenStruct.new request: request_stub
-      end
+    test "can access context's helper methods" do
+      context  = OpenStruct.new helper_method: 'assert_me'
+      renderer = ChildMenuRenderer.new(context)
 
-      context 'an explicit path is not supplied' do
-        should 'get the path from the request' do
-          renderer = MenuRenderer.new(ContextStub, nil)
-          assert_equal Path.new(:post, '/path'), renderer.current_path
+      assert_equal 'assert_me', renderer.helpers.helper_method
+    end
+
+    context '#build_html' do
+      context 'html_safe is available on strings' do
+        should 'call html_safe on returned string' do
+          renderer = ChildMenuRenderer.new(proc {})
+          output = renderer.build_html do |html|
+            OpenStruct.new html_safe: 'I was called'
+          end
+
+          assert_equal 'I was called', output
         end
       end
 
-      context 'an explicit path is supplied' do
-        should 'not get the path from the request' do
-          renderer = MenuRenderer.new(ContextStub, '/other')
-          assert_equal Path.new(:get, '/other'), renderer.current_path
+      context "html_safe isn't available on strings" do
+        should "not call html_safe" do
+          renderer = ChildMenuRenderer.new(proc {})
+          stub     = Object.new
+
+          output = renderer.build_html { |_| stub }
+          assert_equal stub, output
+        end
+      end
+    end
+
+    test '#build_menu runs the supplied block for each menu item' do
+      renderer = ChildMenuRenderer.new(proc {})
+      renderer.menu = Menu.new(renderer) do |m|
+        m.add 'Item 1', '1'
+        m.add 'Item 2', '2'
+      end
+
+      items = []
+
+      renderer.build_menu do |item, accumulator|
+        items << item
+        'should return rendered item string'
+      end
+
+      assert_equal %w[1 2], items.map(&:path)
+    end
+
+    context 'child renderer rules and functionality' do
+      should 'place main logic inside a .render class block' do
+        child_renderer_class = Class.new(MenuRenderer) do
+          render { 'render_me' }
+        end
+
+        child_renderer = child_renderer_class.new(proc {})
+        assert_equal 'render_me', child_renderer.render
+      end
+
+      context 'a .render call is required on child class definition' do
+        should 'define a #render method and protect the final output' do
+          child_renderer_class = Class.new(MenuRenderer) do
+            render { OpenStruct.new html_safe: 'assert_me' }
+          end
+
+          child_renderer = child_renderer_class.new(proc {})
+          assert_equal 'assert_me', child_renderer.render
+        end
+      end
+    end
+
+    context '#current_path' do
+      context 'supplied context has a request' do
+        ContextStub = begin
+          request_stub = Class.new do
+            def method; 'POST'  end
+            def path;   '/path' end
+          end.new
+
+          OpenStruct.new request: request_stub
+        end
+
+        context 'an explicit path is not supplied' do
+          should 'get the path from the request' do
+            renderer = ChildMenuRenderer.new(ContextStub, nil)
+            assert_equal Path.new(:post, '/path'), renderer.current_path
+          end
+        end
+
+        context 'an explicit path is supplied' do
+          should 'not get the path from the request' do
+            renderer = ChildMenuRenderer.new(ContextStub, '/other')
+            assert_equal Path.new(:get, '/other'), renderer.current_path
+          end
         end
       end
     end
